aca2927df786941213b2e027e358d990085c7401
Ahsan Ali <mrahsanali5@gmail.com>
2020-10-16 18:19:43 +0500

    initial_commit

---
+++ firebase_auth_web/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\flutter_windows_v1.12.13+hotfix.5-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\AhsanAli\Developer\flutterfire-1\packages\firebase_auth\firebase_auth_web"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\flutter_windows_v1.12.13+hotfix.5-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=0.3.1"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
